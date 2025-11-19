30521f2afa1c2de65c0ada63b08bf5e2dd8d424d
zhannaderk <Zhanna.Orlenko-ext@ab-Inbev.com>
2025-11-19 20:20:44 +0200

    second commit

--- home_page.py
+++ home_page.py
@@ -1,7 +1,5 @@
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
 import time
 
 driver = webdriver.Chrome()
